cc3a3e273d70441e955b22066a62f63ae600565d
Gage Henderson <gage@quangoinc.com>
2018-08-27 14:24:00 -0700

    Basic functionality, need to make it more dynamic and add more words tho

---
+++ index.js
@@ -0,0 +1,40 @@
+var words = require('./words.js');
+
+// Get a random number from 0 to the amount
+// specified.
+function getRandom(amount) {
+  return Math.floor(Math.random() * amount);
+}
+
+// Get a word from the type's list (ie, noun,
+// adjective).
+function getWord(type) {
+  var list = words[type];
+  return list[getRandom(list.length)];
+}
+
+// Generate the name
+function generateName() {
+  var name = '';
+  var hasTwoNouns = getRandom(2);
+  var adjective = getWord('adjectives');
+
+  // The adjective
+  name += adjective;
+
+  // The first noun
+  name += ' ' + getWord('nouns');
+
+  // Inserting a second noun
+  if (hasTwoNouns == 1) {
+    name += ' ' + getWord('nouns');
+  }
+
+  return name;
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log(generateName());
+}
+
+module.exports = generateName;
---
+++ words.js
@@ -0,0 +1,29 @@
+var words = [];
+
+// Adjectives (most of the time).
+words['adjectives'] = ['Young', "Lil'", 'Big', 'YBN', 'Sick', 'Ill', 'Wavy'];
+
+// Nouns (most of the time).
+words['nouns'] = [
+  'Wizard',
+  'Fork',
+  'Mustard',
+  'Gambino',
+  'Strap',
+  'Chopper',
+  'Baby',
+  'Thumb',
+  'Poppa',
+  'Dripper',
+  'Purple',
+  'Clout',
+  'Icey',
+  'Nascar',
+  'Shooter',
+  'Xan',
+  'Emo',
+  'dookey',
+  'God'
+];
+
+module.exports = words;
